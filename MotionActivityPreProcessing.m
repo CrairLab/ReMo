bc37a964d3d2583c3a6afac95a51c185effa277f
YixiangWebsterWang <yixiang.wang@yale.edu>
2022-01-21 18:04:21 -0500

    Update MotionActivityPreProcessing.m

--- MotionActivityPreProcessing.m
+++ MotionActivityPreProcessing.m
@@ -8,29 +8,35 @@ function MotionActivityPreProcessing(cur_folder, downFactor)
 
     cd(cur_folder);
     disp(['Working on ' cur_folder]);
-    
-    [~, ~, A4] = ReadAndGetAvg(downFactor, 1);
-    
-    % Photobleaching correction
-    
-    A5 = movieData.bleachCorrection(A4);
-    
-    clear A4
 
-    % Gaussian smoothing
-    A6 = movieData.GauSmoo(A5, 1); % Sigma = 1
+    savefn = ['Combined_downsampled_' num2str(downFactor) '_filtered.mat'];
     
-    clear A5
+    if ~exist(savefn, 'file')
     
-    % dFoF
-    A_dFoF = movieData.grossDFoverF(A6, 1, 10);
-    A_dFoF = single(A_dFoF);
-    
-    clear A6
-    
-    savefn = ['Combined_downsampled_' num2str(downFactor) '_filtered'];
-    
-    save([savefn '.mat'], 'A_dFoF', '-v7.3');
+        [~, ~, A4] = ReadAndGetAvg(downFactor, 1);
+
+        % Photobleaching correction
+
+        A5 = movieData.bleachCorrection(A4);
+
+        clear A4
+
+        % Gaussian smoothing
+        A6 = movieData.GauSmoo(A5, 1); % Sigma = 1
+
+        clear A5
+
+        % dFoF
+        A_dFoF = movieData.grossDFoverF(A6, 1, 10);
+        A_dFoF = single(A_dFoF);
+
+        clear A6
+
+        save(savefn, 'A_dFoF', '-v7.3');
+    else
+        load(savefn)
+        disp('Loading existing A_dFoF (filtered matrix)...')
+    end
     
     disp('Doing seed-based correlation analysis...')
     movieData.SeedBasedCorr_GPU(A_dFoF, downFactor, 1000);
